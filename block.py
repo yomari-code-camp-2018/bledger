6d7dd0287c24a2b8bab537cb7b2079d2407040c8
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2018-02-02 14:17:41 +0545

    Implement ask email

--- block.py
+++ block.py
@@ -2,18 +2,18 @@ import hashlib as hasher
 import datetime as date
 import gnupg as gpg
 import os
+from sys import exit
 import json
 import hexdump
+from pathlib import Path
 
-gpg = gpg.GPG('/home/dineshdb/gpgtest')
-gpg.encoding = 'utf-8'
+gpg_home = os.path.join(Path.home(), 'gpgtests')
 
 class Block:
     def __init__(self, index, timestamp, frm, to, previous_hash):
         
         self.index = index
         self.timestamp = timestamp
-        self.data = data
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
         pass
@@ -22,34 +22,33 @@ class Block:
         sha = hasher.sha256()
         sha.update((str(self.index) + 
                    str(self.timestamp) + 
-                   str(self.data) + 
                    str(self.previous_hash)).encode('utf-8'))
         return sha.hexdigest()
        
     def sign():
         pass
+
+def create_genesis_block():
+    return Block(0, date.datetime.now(), "0", "0", "0")
     
 class BlockChain:
     def __init__(self, blocks = []):
-        this.blocks = blocks
-        this.keys = []
-        if len(this.blocks) == 0:
-            this.blocks = [create_genesis_block()]
-        this.index = len(this.blocks)
+        self.blocks = blocks
+        self.keys = []
+        if len(self.blocks) == 0:
+            self.blocks = [create_genesis_block()]
+        self.index = len(self.blocks)
         pass
     
-    def create_genesis_block():
-        return Block(0, date.datetime.now(), "0", "0", "0")
-    
     def new_transaction(self, frm, to, amount, comment):        
-        block = Block(this.index+1, date.datetime.now(), frm, to, this.blocks[this.index].hash)
+        block = Block(self.index+1, date.datetime.now(), frm, to, self.blocks[self.index].hash)
         
     def add_block():
         pass
     
         
     def get_block(self, index):
-        if index > len(this.blocks):
+        if index > len(self.blocks):
             pass
         
     def generate_key(email, pas):
@@ -70,7 +69,7 @@ class BlockChain:
         pass
         
         
-    def generate_key(email, pas):
+    def generate_key(self, email, pas):
         input_data =  gpg.gen_key_input(
             name_email = email,
             passphrase = pas
@@ -80,11 +79,33 @@ class BlockChain:
         json_dump = json.dumps({"email" : email, "key" : str(key)})
         return json_dump
 
-def list_keys():
-    gpg.list_keys()
+    def remove_keys():
+        pass
+        
+def find_available_keys():
     pass
-
-
-
-def remove_keys():
+    
+def get_key(key):
     pass
+    
+def delete_keys():
+    gpg.delete_keys(fp)
+
+chain = BlockChain()
+gpg = gpg.GPG( homedir = gpg_home)
+gpg.encoding = 'utf-8'
+
+keys = gpg.list_keys()
+
+if len(keys) == 0:
+    q = input("No keys found, would you like to generate one?")
+    if q != "y":
+    	quit(1)
+    
+    email = input("Email : ")  
+    key = chain.generate_key(email, "")
+    
+email = input("Email : ")
+    
+print("Using email " + email)
+
